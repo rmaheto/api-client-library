2489bbcf0c769ffdec2e52f98226f30e6597c7eb
codemaniac <rmkaheto@gmail.com>
2023-12-20 05:00:26 -0600

    inject logging interceptor

--- src/main/java/com/codemaniac/codemaniacrestclientlibrary/RestClientConfig.java
+++ src/main/java/com/codemaniac/codemaniacrestclientlibrary/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.codemaniac.codemaniacrestclientlibrary;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -10,12 +11,19 @@ import java.util.List;
 @Configuration
 public class RestClientConfig {
 
+    private final LoggingRequestInterceptor loggingRequestInterceptor;
+
+    @Autowired
+    public RestClientConfig(LoggingRequestInterceptor loggingRequestInterceptor) {
+        this.loggingRequestInterceptor = loggingRequestInterceptor;
+    }
+
     @Bean
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
 
         List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
-        interceptors.add(new LoggingRequestInterceptor());
+        interceptors.add(loggingRequestInterceptor);
         restTemplate.setInterceptors(interceptors);
 
         return restTemplate;
